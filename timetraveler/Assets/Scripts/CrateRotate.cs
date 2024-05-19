21e7bf2b245af656ec8fb03a90ae7f5606b07589
berke-ozcan <142122706+berke-ozcan@users.noreply.github.com>
2024-05-19 19:55:57 +0300

    FUTURİSTİC CRATE YAPILDI

---
+++ timetraveler/Assets/Scripts/CrateRotate.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateRotate : MonoBehaviour
+{
+    // Event declaration
+    public static event Action<string, int> Rotated;
+
+    private bool coroutineAllowed;
+    private int numberShown;
+
+    private void Start()
+    {
+        coroutineAllowed = true;
+        numberShown = 0;
+    }
+
+
+    //private void OnMouseDown()
+    //{
+    //    Debug.Log("asdfdsafdsafad xdxdxd");
+    //    int randomNumber = UnityEngine.Random.Range(1, 10); // Örnek bir rasgele sayı oluştur
+    //    Rotated?.Invoke(gameObject.name, randomNumber); // Rotated olayını tetikle
+    //}
+
+    //private void OnMouseDown()
+    //{
+    //    if (coroutineAllowed)
+    //    {
+    //        StartCoroutine("RotateWheel");
+    //    }
+    //}
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Change 0 to 1 for right mouse button
+        {
+            // Get the main camera
+            Camera mainCamera = Camera.main;
+
+            // Check if the main camera exists
+            if (mainCamera != null)
+            {
+                // Get the mouse position
+                Vector3 mousePosition = Input.mousePosition;
+
+                // Create a ray from the camera through the mouse position
+                Ray ray = mainCamera.ScreenPointToRay(mousePosition);
+
+                // Create a RaycastHit variable to store information about the hit
+                RaycastHit hit;
+
+                // Perform the raycast
+                if (Physics.Raycast(ray, out hit))
+                {
+                    // If the ray hits something, you can access information about the hit
+                    Debug.Log("Hit object: " + hit.collider.gameObject.name);
+                    Debug.Log("Hit point: " + hit.point);
+                    Debug.Log("Hit normal: " + hit.normal);
+
+                    if(gameObject.name == hit.collider.gameObject.name)
+                        if (coroutineAllowed)
+                            {
+                                StartCoroutine("RotateWheel");
+                            }
+                }
+                else
+                {
+                    // If the ray doesn't hit anything, you can handle this case here
+                    Debug.Log("No object hit.");
+                }
+            }
+        }
+    }
+
+
+    private IEnumerator RotateWheel()
+    {
+        coroutineAllowed = false;
+
+        for (int i = 0; i <= 11; i++) {
+
+            transform.Rotate(new Vector3(-3.75f, 0f, 0f));
+
+            yield return new WaitForSeconds(0.01f);
+        }
+        coroutineAllowed = true;
+
+        numberShown += 1;
+
+        if (numberShown > 7)
+        {
+            numberShown = 0;
+        }
+
+        Rotated(name, numberShown);
+    }
+}
---
+++ timetraveler/Assets/Scripts/Cratelock.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateLockControl : MonoBehaviour
+{
+    private int[] result, correctCombination;
+    public Animator animator;
+
+    private void Start()
+    {
+        result = new int[] { 0, 0, 0, 0, 0, 0 }; // 5, 5, 5 varsayılan değerlerle başlatıldı
+        correctCombination = new int[] { 4, 5, 2, 1, 7, 3 }; // Doğru kombinasyon ayarlandı
+
+        // Rotate.Rotated olayına CheckResults metodu eklendi
+        CrateRotate.Rotated += CheckResults;
+    }
+
+    private void CheckResults(string wheelName, int number)
+    {
+        switch (wheelName)
+        {
+            case "egyptianwheel1": // "wheell" yerine "wheel1" düzeltildi
+                result[0] = number; // Doğru indeks düzeltildi
+                break;
+            case "egyptianwheel2":
+                result[1] = number;
+                break;
+            case "egyptianwheel3":
+                result[2] = number;
+                break;
+            case "egyptianwheel4":
+                result[3] = number;
+                break;
+            case "egyptianwheel5":
+                result[4] = number;
+                break;
+            case "egyptianwheel6":
+                result[5] = number;
+                break;
+        }
+
+        // Doğru kombinasyon kontrol edildi ve konsola "Opened!" yazdırıldı
+        if (result[0] == correctCombination[0] && result[1] == correctCombination[1] && result[2] == correctCombination[2] 
+         && result[3] == correctCombination[3] && result[4] == correctCombination[4] && result[5] == correctCombination[5])
+        {
+            Debug.Log("Opened!");
+            animator.SetBool("CrateOpened", true);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // OnDestroy() çağrıldığında Rotate.Rotated olayından CheckResults metodunu kaldır
+        CrateRotate.Rotated -= CheckResults;
+    }
+}
