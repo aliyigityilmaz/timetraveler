add05d2de4b3a336165a39e0b5fdf5bb10914d86
Ali Yiğit Yılmaz <redkom.yigit@gmail.com>
2024-05-20 21:42:09 +0300

    mainmenu

---
+++ timetraveler/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
